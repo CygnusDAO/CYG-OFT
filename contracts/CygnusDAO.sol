042c6fb8e27dd69c1bc21d45c278702e48923d1e
0xHyoga <0xHyoga@cygnusdao.finance>
2023-08-21 17:37:10 +0200

    final CYG token OFT

--- contracts/CygnusDAO.sol
+++ contracts/CygnusDAO.sol
@@ -2,23 +2,26 @@
 pragma solidity >=0.8.17;
 
 // Dependencies
-import {OFTV2} from "./OFTV2.sol";
+import {IERC20, OFT} from "@layerzerolabs/solidity-examples/contracts/token/oft/OFT.sol";
 
 /**
- *  @title CygnusDAO CYG token built as layer-zero`s OFTV2.
+ *  @title CygnusDAO CYG token built as layer-zero`s OFT.
  *  @notice On each chain the CYG token is deployed there is a cap of 2.5M to be minted over 42 epochs (4 years).
  *          See https://github.com/CygnusDAO/cygnus-token/blob/main/contracts/cygnus-token/PillarsOfCreation.sol
  *          Instead of using `totalSupply` to cap the mints, we must keep track internally of the total minted
- *          amount, to not break compatability with the OFTV2's `_debitFrom` and `_creditTo` functions (since these
+ *          amount, to not break compatability with the OFT's `_debitFrom` and `_creditTo` functions (since these
  *          burn and mint supply into existence respectively).
  */
-contract CygnusDAO is OFTV2 {
+contract CygnusDAO is OFT {
     /*  ═══════════════════════════════════════════════════════════════════════════════════════════════════════ 
             1. EVENTS AND ERRORS
         ═══════════════════════════════════════════════════════════════════════════════════════════════════════  */
 
     /// @custom:event NewCYGMinter Emitted when the CYG minter contract is set, only emitted once
     event NewCYGMinter(address oldMinter, address newMinter);
+
+    /// @custom:event Migrate Emitted when a user migrates from the old CYG
+    event Migrate(address user, uint256 amount);
 
     /// @custom:error ExceedsSupplyCap Reverts when minting above cap
     error ExceedsSupplyCap();
@@ -28,6 +31,9 @@ contract CygnusDAO is OFTV2 {
 
     /// @custom:error OnlyPillars Reverts when msg.sender is not the CYG minter contract
     error OnlyPillars();
+
+    /// @custom:error InsufficientBalance Reverts if already migrated or has no balance
+    error InsufficientBalance();
 
     /*  ═══════════════════════════════════════════════════════════════════════════════════════════════════════ 
             2. STORAGE
@@ -46,21 +52,16 @@ contract CygnusDAO is OFTV2 {
             3. CONSTRUCTOR
         ═══════════════════════════════════════════════════════════════════════════════════════════════════════  */
 
-    /// @notice Constructs the CYG OFTV2 token and gives sender initial ownership to set paths.
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        uint8 _sharedDecimals,
-        address _lzEndpoint
-    ) OFTV2(_name, _symbol, _sharedDecimals, _lzEndpoint) {
-        // Every chain deployment is the same, 250,000 inital mint, 2,250,000 to pillars
+    /// @notice Constructs the CYG OFT token and gives sender initial ownership to set paths.
+    constructor(string memory _name, string memory _symbol, address _lzEndpoint) OFT(_name, _symbol, _lzEndpoint) {
+        // Every chain deployment is the same, 250,000 inital mint rest to pillars
         uint256 initial = 250_000e18;
 
         // Increase initial minted
         totalMinted += initial;
 
         // Mint initial supply to admin
-        _mint(msg.sender, initial);
+        _mint(_msgSender(), initial);
     }
 
     /*  ═══════════════════════════════════════════════════════════════════════════════════════════════════════ 
@@ -96,6 +97,7 @@ contract CygnusDAO is OFTV2 {
     /// @notice Only the `pillarsOfCreation` contract may mint
     /// @param to The receiver of the CYG token
     /// @param amount The amount of CYG token to mint
+    /// @custom:security only-pillars-contract
     function mint(address to, uint256 amount) external onlyPillars {
         // Gas savings
         uint256 _totalMinted = totalMinted + amount;
@@ -112,6 +114,7 @@ contract CygnusDAO is OFTV2 {
 
     /// @notice Assigns the only contract on the chain that can mint the CYG token. Can only be set once.
     /// @param _pillars The address of the minter contract
+    /// @custom:security only-admin
     function setPillarsOfCreation(address _pillars) external onlyOwner {
         // Current CYG minter
         address currentPillars = pillarsOfCreation;
@@ -121,5 +124,36 @@ contract CygnusDAO is OFTV2 {
 
         /// @custom:event NewCYGMinter
         emit NewCYGMinter(currentPillars, pillarsOfCreation = _pillars);
+    }
+
+    /* ──────── Only for Polygon Mainnet ──────── */
+
+    /// @notice Old CYG token
+    address public constant OLD_CYGNUS_TOKEN = 0xc115521DC2D0F950AD5D3589D0a4b22239C56A1B;
+
+    /// @notice Migrate old CYG to OFT
+    function migrate() external {
+        // Get migrator
+        address migrator = _msgSender();
+
+        // Get current balance
+        uint256 balance = IERC20(OLD_CYGNUS_TOKEN).balanceOf(migrator);
+
+        // Check balance
+        if (balance > 0) {
+            // Burn old CYG to zero address
+            IERC20(OLD_CYGNUS_TOKEN).transferFrom(migrator, address(0), balance);
+
+            // Increase
+            totalMinted += balance;
+
+            // Mint to migrator
+            _mint(migrator, balance);
+        }
+        // Revert if user has no balance
+        else revert("Insufficient Balance");
+
+        /// @custom:event Migrate
+        emit Migrate(migrator, balance);
     }
 }
